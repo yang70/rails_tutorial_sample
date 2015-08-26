51dd58c554be4e038117b622d3e63dcaac9423ad
Matthew Yang <yang70@gmail.com>
2015-08-26 15:40:21 -0700

    Finish log in/log out

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,52 @@
+module SessionsHelper
+
+  # logs in the given user
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  # returns the user corresponding to the remember token cookie
+  def current_user
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
+  end
+
+  # returns true if user logged in, else false
+  def logged_in?
+    !current_user.nil?
+  end
+
+  #logs out the current user
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
+
+  # Remembers a user in a persistent session
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  # Forgets a persistent session.
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
+  # Logs out the current user.
+  def log_out
+    forget(current_user)
+    session.delete(:user_id)
+    @current_user = nil
+  end
+end
