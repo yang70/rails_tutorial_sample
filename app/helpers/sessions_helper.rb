ec2ad1120253e57978b8b5711b820706028dbee0
Matthew Yang <yang70@gmail.com>
2015-08-29 06:35:19 -0700

    continue working on users

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -18,6 +18,11 @@ module SessionsHelper
     end
   end
 
+  # Returns true if the given user is the current user.
+  def current_user?(user)
+    user == current_user
+  end
+
   # returns true if user logged in, else false
   def logged_in?
     !current_user.nil?
@@ -48,5 +53,16 @@ module SessionsHelper
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
+  end
+
+  # Redirects to stored location (or to the default).
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # Stores the URL trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.url if request.get?
   end
 end
